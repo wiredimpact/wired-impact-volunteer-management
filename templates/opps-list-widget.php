f1c9dc3699732ffc5d1224daf434f9790a9b70b2
Daniel Waldschmidt <danielpwaldschmidt@gmail.com>
2016-05-03 13:06:06 -0500

    Convert ternary operator into if/else statements and update comments for clarity

--- templates/opps-list-widget.php
+++ templates/opps-list-widget.php
@@ -24,15 +24,29 @@ $opp = new WI_Volunteer_Management_Opportunity( $post->ID ); //Get volunteer opp
    the_title( sprintf( '<a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a>' );
 
    // Display 'When' information IF option to show 'When' is checked in widget settings &&
-   // IF one time opp OR flexible opp with date filled out
-   ( $display_opp_when && ( $opp->opp_meta['one_time_opp'] || strlen( $opp->opp_meta['flexible_frequency'] ) > 0 ) ) ? $opp->display_meta( ( $opp->opp_meta['one_time_opp'] == 1 ) ? $opp->format_opp_times() : $opp->opp_meta['flexible_frequency'], __( 'When:', 'wired-impact-volunteer-management' ) ) : null;
+   // opportunity is a one-time opp OR flexible opp with date filled out
+   if ( $display_opp_when === true && ( $opp->opp_meta['one_time_opp'] === 1 || strlen( $opp->opp_meta['flexible_frequency'] ) > 0 ) ) {
+
+      // IF one time-opportunity display formatted dates/times
+      // ELSE we know it's a flexible opportunity so display flexible frequency
+      if( $opp->opp_meta['one_time_opp'] === 1 ) {
+         $opp->display_meta( $opp->format_opp_times(), __( 'When:', 'wired-impact-volunteer-management' ) );
+      } else {
+         $opp->display_meta( $opp->opp_meta['flexible_frequency'], __( 'When:', 'wired-impact-volunteer-management' ) );
+      }
+
+   }
 
    // Display 'Where' information IF option to show 'Where' is checked in widget settings &&
-   // IF any of the Location fields are filled out
-   ( $display_opp_where && strlen( $opp->format_address() ) > 0 ) ? $opp->display_meta( $opp->format_address(),           __( 'Where:', 'wired-impact-volunteer-management' ) ) : null;
+   // any of the Location fields are filled out
+   if ( $display_opp_where === true && strlen( $opp->format_address() ) > 0 ) {
+      $opp->display_meta( $opp->format_address(),           __( 'Where:', 'wired-impact-volunteer-management' ) );
+   }
 
    // Display number of open volunteer spots IF option to show 'Open Volunteer Spots' is checked in widget settings
-   $display_opp_spots ? $opp->display_meta( $opp->get_open_volunteer_spots(),    __( 'Open Volunteer Spots:', 'wired-impact-volunteer-management' ) ) : null;
+   if ( $display_opp_spots === true ) {
+      $opp->display_meta( $opp->get_open_volunteer_spots(),    __( 'Open Volunteer Spots:', 'wired-impact-volunteer-management' ) );
+   }
 
 ?>
 
