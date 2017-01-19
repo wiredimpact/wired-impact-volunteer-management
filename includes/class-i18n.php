28f2754ebc75a19e64525e07f2f1ff92c490000b
Jonathan Goldford <jonathan@wiredimpact.com>
2017-01-19 16:24:18 -0600

    Fixed bug where a .mo language file wasn't being loaded when placed in the correct location.
    I was able to test this by building a translation file using the Loco
    Translate plugin. It created a .mo file for the fr_FR (French) locale
    within the languages folder. Once this bug was fixed that translation
    file loaded as expected.

--- includes/class-i18n.php
+++ includes/class-i18n.php
@@ -41,7 +41,7 @@ class WI_Volunteer_Management_i18n {
 	 */
 	public function load_plugin_textdomain() {
 
-		load_plugin_textdomain( $this->domain );
+		load_plugin_textdomain( $this->domain, false, dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/' );
 
 	}
 
