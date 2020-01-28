3b7d8aa7a95a8db8a2b6a0e69cedf99a3e6faf3f
Dan <dan@wiredimpact.com>
2020-01-28 16:20:27 -0600

    Reduce the Volunteer Opportunities block transform property to one 'from' declaration that combines the tags for one time and flexible opportunities.

--- admin/js/wi-volunteer-management-block.js
+++ admin/js/wi-volunteer-management-block.js
@@ -50,40 +50,9 @@
 		
 		transforms: 	{
 							from: [
-								/*{
-									/**
-									 * @todo 	We should be able to use this transform and remove
-									 * 			the other two at some point in the near future.
-									 * 			I posted about a bug where including two shortcodes
-									 * 			in the "tag" property doesn't appear to be working.
-									 * 			You can see the issue at https://github.com/WordPress/gutenberg/issues/14476.
-									 */ /*
+								{
 									type: 'shortcode',
 									tag: [ 'one_time_volunteer_opps', 'flexible_volunteer_opps' ],
-									attributes: {
-										showOneTime: {
-											type: 'boolean',
-											shortcode: ( attributes, { shortcode } ) => {
-												return shortcode.tag === 'one_time_volunteer_opps' ? true : false;
-											},
-										}
-									},
-								}, */
-								{
-									type: 'shortcode',
-									tag: 'one_time_volunteer_opps',
-									attributes: {
-										showOneTime: {
-											type: 'boolean',
-											shortcode: ( attributes, { shortcode } ) => {
-												return shortcode.tag === 'one_time_volunteer_opps' ? true : false;
-											},
-										}
-									},
-								},
-								{
-									type: 'shortcode',
-									tag: 'flexible_volunteer_opps',
 									attributes: {
 										showOneTime: {
 											type: 'boolean',
